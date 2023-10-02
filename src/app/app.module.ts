58cff6701488f39622ce1273b0465eae7e117fc2
claudia <claudia.iacob@hotmail.com>
2023-10-02 18:14:54 +0200

    Nav y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContainerComponent } from './container/container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
